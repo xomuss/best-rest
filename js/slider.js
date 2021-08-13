782c77a3692c6132d9553c46209cfdb47274f619
Alexander Khomenko <72103285+xomuss@users.noreply.github.com>
2021-08-13 12:49:36 +0300

    слайдер

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+const data = [
+    {
+        text: 'Thanks to BestRest,I met new friends & visited Europe for a penny!',
+        img: '',
+        name: '',
+        city: '',
+        rating: ''
+    }
+]
